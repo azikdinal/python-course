322e2a12fe6d9653735dbf4581c8c83ef7b7f4fe
Azamat <dinaev2002@gmail.com>
2024-12-24 23:25:15 +0300

    update

--- database.py
+++ database.py
@@ -1,7 +1,23 @@
+from dotenv import load_dotenv
+import os
 import psycopg2
 
-# Установление соединения с базой данных
-connection = psycopg2.connect("database=flask, user=flask, password=flask")
+# использовать .env файл
+load_dotenv()
 
+# Подключение к базе данных
+connection = psycopg2.connect(
+    host=os.getenv("DB_HOSTNAME"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD")
+)
 
 cursor = connection.cursor()
+
+cursor.execute("SELECT * from users;")
+
+# Закрыть соединение с БД
+cursor.close()
+connection.close()
